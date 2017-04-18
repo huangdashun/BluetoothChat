11172bbb8f9da21c910958bc87997144e7116d31
huangshun <406846759@qq.com>
2017-04-18 19:57:35 +0800

    add record list

--- app/src/main/java/huangshun/it/com/btproject/utils/NotificationUtil.java
+++ app/src/main/java/huangshun/it/com/btproject/utils/NotificationUtil.java
@@ -10,32 +10,21 @@ import android.content.Intent;
 import huangshun.it.com.btproject.R;
 
 public class NotificationUtil {
-	public static final int NOTIFY_ID1 = 1001;
-	
-	public static void notifyMessage(Context context, String msg, Activity activity){
-		//Notification builder; 
-		PendingIntent contentIntent = null; 
-		NotificationManager nm;
-		// 发送通知需要用到NotificationManager对象 
-		nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-		// 消息对象
+    public static final int NOTIFY_ID1 = 1001;
+
+    public static void notifyMessage(Context context, String msg, Activity activity) {
+        //Notification builder;
+        PendingIntent contentIntent = null;
+        NotificationManager nm;
+        // 发送通知需要用到NotificationManager对象
+        nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // 消息对象
         Intent notificationIntent = new Intent(context, activity.getClass());
-		notificationIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        // PendingIntent.getActivity(Context context, int requestCode, Intent intent, int flags)
-        // 用来获得一个挂起的PendingIntent，让该Intent去启动新的Activity来处理通知
-        contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0); 
-
-
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
         //定义通知栏展现的内容信息
         int icon = R.drawable.icon;
         long when = System.currentTimeMillis();
-//        Notification notification = new Notification(icon, msg, when);
-//        notification.defaults |= Notification.DEFAULT_VIBRATE;
-//        notification.defaults |= Notification.DEFAULT_SOUND; // 调用系统自带声音
-//        notification.flags |= Notification.FLAG_AUTO_CANCEL; // 点击清除按钮或点击通知后会自动消失
-//        notification.defaults |= Notification.DEFAULT_LIGHTS;
-//        notification.vibrate = new long[]{300, 500};
-//        notification.setLatestEventInfo(context, "BluetoothChat", msg, contentIntent);
         Notification.Builder builder = new Notification.Builder(context);
         Notification notification = builder.setContentIntent(contentIntent)
                 .setSmallIcon(icon)
@@ -48,20 +37,7 @@ public class NotificationUtil {
                 .setContentTitle("BluetoothChat")
                 .setContentText(msg)
                 .setAutoCancel(true).build();
-       /* // 定制我们要在状态栏显示的通知样式
-		builder = new Notification(context);
-		builder.setContentIntent(contentIntent)
-		 	.setSmallIcon(R.drawable.ic_launcher)//设置状态栏里面的图标（小图标） 　　　　　　　　　　　　　　　　　　　　.setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.i5))//下拉下拉列表里面的图标（大图标） 　　　　　　　.setTicker("this is bitch!") //设置状态栏的显示的信息
-		 	.setWhen(System.currentTimeMillis())//设置时间发生时间
-		 	.setAutoCancel(true)//设置可以清除
-		 	.setContentTitle("This is ContentTitle")//设置下拉列表里的标题
-		 	.setContentText("this is ContentText");//设置上下文内容
-        */		
-		// 获得刚才创建的通知对象
-		// Notification notification = builder.getNotification();//获取一个Notification
-	     
-	     // 通过NotificationManger来发送通知消息
-	     // 参数1通知的ID，参数2发送哪个通知
-	     nm.notify(NOTIFY_ID1, notification);
-	}
+        // 参数1通知的ID，参数2发送哪个通知
+        nm.notify(NOTIFY_ID1, notification);
+    }
 }
